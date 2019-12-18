c8743fd483e9889dfdec010ec369e6d3807d4ce8
Luis Oyanguren <leog.1992@gmail.com>
2019-12-18 14:10:22 -0500

    eliminar venta

--- procesos/del_compra.php
+++ procesos/del_compra.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 require '../class/cl_compra.php';
 require '../class/cl_compra_pago.php';
 require '../class/cl_banco_movimiento.php';
