29707ab5b023cdd3b2189a70e86ad6c11aef24e4
Ruben <hruben0214@gmail.com>
2021-04-03 13:11:41 -0500

    cambio al registro de apertura de caja
    Se agrego la variable para cargar internamente la sucursal

--- procesos/reg_apertura_caja.php
+++ procesos/reg_apertura_caja.php
@@ -4,6 +4,7 @@ session_start();
 require '../class/cl_caja_diaria.php';
 $c_caja = new cl_caja_diaria();
 $c_caja->setIdEmpresa($_SESSION['id_empresa']);
+$c_caja->setIdSucursal($_SESSION['id_sucursal']);
 $c_caja->setFecha(date("Y-m-d"));
 $c_caja->setMApertura(filter_input(INPUT_POST, 'input_apertura'));
 
